LarrMarburger/puppet-lint

commit f8c002a2c6e11008c39e5926bdbb12d09afe8fa2

Rewrite double quoted string handling for nested interpolation

lib/puppet-lint/lexer.rb
@@ -2,8 +2,9 @@
 
 require 'pp'
 require 'strscan'
-require 'puppet-lint/lexer/token'
 require 'set'
+require 'puppet-lint/lexer/token'
+require 'puppet-lint/lexer/string_slurper'
 
 class PuppetLint
   # Internal: A generic error thrown by the lexer when it encounters something
@@ -28,11 +29,15 @@ class PuppetLint
       @column = column
       @reason = reason
     end
+
+    def to_s
+      "PuppetLint::LexerError: Line:#{line_no} Column: #{column} Reason: #{reason}"
+    end
   end
 
   # Internal: The puppet-lint lexer. Converts your manifest into its tokenised
   # form.
-  class Lexer # rubocop:disable Metrics/ClassLength
+  class Lexer
     def initialize
       @line_no = 1
       @column = 1
@@ -215,7 +220,12 @@ class PuppetLint
 
         if var_name = chunk[%r{\A\$((::)?(\w+(-\w+)*::)*\w+(-\w+)*(\[.+?\])*)}, 1]
           length = var_name.size + 1
-          tokens << new_token(:VARIABLE, var_name)
+          opts = if chunk.start_with?('$')
+                   { :raw => "$#{var_name}" }
+                 else
+                   {}
+                 end
+          tokens << new_token(:VARIABLE, var_name, opts)
 
         elsif chunk =~ %r{\A'.*?'}m
           str_content = StringScanner.new(code[i + 1..-1]).scan_until(%r{(\A|[^\\])(\\\\)*'}m)
@@ -223,10 +233,14 @@ class PuppetLint
           tokens << new_token(:SSTRING, str_content[0..-2])
 
         elsif chunk.start_with?('"')
-          str_contents = slurp_string(code[i + 1..-1])
-          lines_parsed = code[0..i].split(LINE_END_RE)
-          interpolate_string(str_contents, lines_parsed.count, lines_parsed.last.length)
-          length = str_contents.size + 1
+          slurper = PuppetLint::Lexer::StringSlurper.new(code[i + 1..-1])
+          begin
+            string_segments = slurper.parse
+            process_string_segments(string_segments)
+            length = slurper.consumed_bytes + 1
+          rescue PuppetLint::Lexer::StringSlurper::UnterminatedStringError
+            raise PuppetLint::LexerError, @line_no, @column, 'unterminated string'
+          end
 
         elsif heredoc_name = chunk[%r{\A@\(("?.+?"?(:.+?)?#{WHITESPACE_RE}*(/.*?)?)\)}, 1]
           heredoc_queue << heredoc_name
@@ -308,22 +322,6 @@ class PuppetLint
       tokens
     end
 
-    def slurp_string(string)
-      dq_str_regexp = %r{(\$\{|(\A|[^\\])(\\\\)*")}m
-      scanner = StringScanner.new(string)
-      contents = scanner.scan_until(dq_str_regexp)
-
-      if scanner.matched.nil?
-        raise LexerError.new(@line_no, @column, 'Double quoted string missing closing quote')
-      end
-
-      until scanner.matched.end_with?('"')
-        contents += scanner.scan_until(%r{\}}m)
-        contents += scanner.scan_until(dq_str_regexp)
-      end
-      contents
-    end
-
     # Internal: Given the tokens already processed, determine if the next token
     # could be a regular expression.
     #
@@ -403,86 +401,34 @@ class PuppetLint
       token
     end
 
-    # Internal: Split a string on multiple terminators, excluding escaped
-    # terminators.
-    #
-    # string      - The String to be split.
-    # terminators - The String of terminators that the String should be split
-    #               on.
-    #
-    # Returns an Array consisting of two Strings, the String up to the first
-    # terminator and the terminator that was found.
-    def get_string_segment(string, terminators)
-      str = string.scan_until(%r{([^\\]|^|[^\\])([\\]{2})*[#{terminators}]+})
-      begin
-        [str[0..-2], str[-1, 1]]
-      rescue
-        [nil, nil]
-      end
-    end
+    def process_string_segments(segments)
+      return if segments.empty?
 
-    # Internal: Tokenise the contents of a double quoted string.
-    #
-    # string - The String to be tokenised.
-    # line   - The Integer line number of the start of the passed string.
-    # column - The Integer column number of the start of the passed string.
-    #
-    # Returns nothing.
-    def interpolate_string(string, line, column)
-      ss = StringScanner.new(string)
-      first = true
-      value, terminator = get_string_segment(ss, '"$')
-      until value.nil?
-        if terminator == '"'
-          if first
-            tokens << new_token(:STRING, value, :line => line, :column => column)
-            first = false
-          else
-            token_column = column + (ss.pos - value.size)
-            tokens << new_token(:DQPOST, value, :line => line, :column => token_column)
-            line += value.scan(LINE_END_RE).size
-            @column = column + ss.pos + 1
-            @line_no = line
-          end
-        else
-          if first
-            tokens << new_token(:DQPRE, value, :line => line, :column => column)
-            first = false
-          else
-            token_column = column + (ss.pos - value.size)
-            tokens << new_token(:DQMID, value, :line => line, :column => token_column)
-            line += value.scan(LINE_END_RE).size
-          end
-          if ss.scan(%r{\{}).nil?
-            var_name = ss.scan(%r{(::)?(\w+(-\w+)*::)*\w+(-\w+)*})
-            if var_name.nil?
-              token_column = column + ss.pos - 1
-              tokens << new_token(:DQMID, '$', :line => line, :column => token_column)
-            else
-              token_column = column + (ss.pos - var_name.size)
-              tokens << new_token(:UNENC_VARIABLE, var_name, :line => line, :column => token_column)
-            end
-          else
-            line += value.scan(LINE_END_RE).size
-            contents = ss.scan_until(%r{\}})[0..-2]
-            raw = contents.dup
-            if contents.match(%r{\A(::)?([\w-]+::)*[\w-]+(\[.+?\])*}) && !contents.match(%r{\A\w+\(})
-              contents = "$#{contents}"
-            end
-            lexer = PuppetLint::Lexer.new
-            lexer.tokenise(contents)
-            lexer.tokens.each do |token|
-              tok_col = column + token.column + (ss.pos - contents.size - 1)
-              tok_line = token.line + line - 1
-              tokens << new_token(token.type, token.value, :line => tok_line, :column => tok_col)
-            end
-            if lexer.tokens.length == 1 && lexer.tokens[0].type == :VARIABLE
-              tokens.last.raw = raw
-            end
-          end
-        end
-        value, terminator = get_string_segment(ss, '"$')
+      if segments.length == 1
+        tokens << new_token(:STRING, segments[0][1])
+        return
       end
+
+      pre_segment = segments.delete_at(0)
+      post_segment = segments.delete_at(-1)
+
+      tokens << new_token(:DQPRE, pre_segment[1])
+      segments.each do |segment|
+        case segment[0]
+        when :INTERP
+          lexer = PuppetLint::Lexer.new
+          lexer.tokenise(segment[1])
+          lexer.tokens.each_with_index do |t, i|
+            type = i.zero? && t.type == :NAME ? :VARIABLE : t.type
+            tokens << new_token(type, t.value, :raw => t.raw)
+          end
+        when :UNENC_VAR
+          tokens << new_token(:UNENC_VARIABLE, segment[1].gsub(%r{\A\$}, ''))
+        else
+          tokens << new_token(:DQMID, segment[1])
+        end
+      end
+      tokens << new_token(:DQPOST, post_segment[1])
     end
 
     # Internal: Tokenise the contents of a heredoc.

.rubocop_todo.yml
@@ -64,7 +64,7 @@ Metrics/MethodLength:
 
 # Offense count: 18
 Metrics/PerceivedComplexity:
-  Max: 25
+  Max: 28
 
 # Offense count: 1
 # Cop supports --auto-correct.

lib/puppet-lint/lexer/string_slurper.rb
@@ -1,0 +1,100 @@
+require 'strscan'
+
+class PuppetLint
+  class Lexer
+    # Document this
+    # TODO
+    class StringSlurper
+      class UnterminatedStringError < StandardError; end
+
+      attr_accessor :scanner
+      attr_accessor :results
+      attr_accessor :interp_stack
+
+      START_INTERP_PATTERN = %r{\$\{}
+      END_INTERP_PATTERN = %r{\}}
+      END_STRING_PATTERN = %r{(\A|[^\\])(\\\\)*"}
+      UNENC_VAR_PATTERN = %r{(\A|[^\\])\$(::)?(\w+(-\w+)*::)*\w+(-\w+)*}
+      ESC_DQUOTE_PATTERN = %r{\\+"}
+
+      def initialize(string)
+        @scanner = StringScanner.new(string)
+        @results = []
+        @interp_stack = []
+        @segment = []
+      end
+
+      def parse
+        until scanner.eos?
+          if scanner.match?(START_INTERP_PATTERN)
+            start_interp
+          elsif scanner.match?(END_INTERP_PATTERN)
+            end_interp
+          elsif interp_stack.empty? && scanner.match?(UNENC_VAR_PATTERN)
+            unenclosed_variable
+          elsif scanner.match?(ESC_DQUOTE_PATTERN)
+            @segment << scanner.scan(ESC_DQUOTE_PATTERN)
+          elsif scanner.match?(END_STRING_PATTERN)
+            end_string
+            break if interp_stack.empty?
+          else
+            read_char
+          end
+        end
+
+        raise UnterminatedStringError if results.empty? && scanner.matched?
+
+        results
+      end
+
+      def read_char
+        @segment << scanner.getch
+      end
+
+      def consumed_bytes
+        scanner.pos
+      end
+
+      def start_interp
+        scanner.skip(START_INTERP_PATTERN)
+
+        if interp_stack.empty?
+          results << [:STRING, @segment.join]
+          @segment = []
+        end
+
+        interp_stack.push(true)
+      end
+
+      def end_interp
+        unless interp_stack.empty?
+          interp_stack.pop
+
+          if interp_stack.empty?
+            results << [:INTERP, @segment.join]
+            @segment = []
+          end
+        end
+
+        scanner.skip(END_INTERP_PATTERN)
+      end
+
+      def unenclosed_variable
+        read_char if scanner.match?(%r{.\$})
+
+        results << [:STRING, @segment.join]
+        results << [:UNENC_VAR, scanner.scan(UNENC_VAR_PATTERN)]
+        @segment = []
+      end
+
+      def end_string
+        if interp_stack.empty?
+          @segment << scanner.scan(END_STRING_PATTERN).gsub!(%r{"\Z}, '')
+          results << [:STRING, @segment.join]
+        else
+          @segment << scanner.scan(END_STRING_PATTERN)
+        end
+      end
+    end
+  end
+end

lib/puppet-lint/plugins/check_whitespace/arrow_alignment.rb
@@ -51,7 +51,7 @@ PuppetLint.new_check(:arrow_alignment) do
             this_arrow_column = param_column[level_idx] + param_length + 1
           else
             this_arrow_column = param_token.column + param_token.to_manifest.length
-            this_arrow_column += 1 unless param_token.type == :DQPOST
+            this_arrow_column += 1
           end
 
           if arrow_column[level_idx] < this_arrow_column

spec/puppet-lint/lexer/string_slurper_spec.rb
@@ -1,0 +1,235 @@
+require 'spec_helper'
+
+describe PuppetLint::Lexer::StringSlurper do
+  describe '#parse' do
+    subject(:segments) { described_class.new(string).parse }
+
+    context 'when parsing an unterminated string' do
+      let(:string) { 'foo' }
+
+      it 'raises an UnterminatedStringError' do
+        expect { segments }.to raise_error(described_class::UnterminatedStringError)
+      end
+    end
+
+    context 'when parsing up to a double quote' do
+      let(:string) { 'foo"bar' }
+
+      it 'returns a single segment up to the double quote' do
+        expect(segments).to eq([[:STRING, 'foo']])
+      end
+
+      context 'and the string is empty' do
+        let(:string) { '"' }
+
+        it 'returns a single empty string segment' do
+          expect(segments).to eq([[:STRING, '']])
+        end
+      end
+
+      context 'and the string contains' do
+        context 'a newline' do
+          let(:string) { %(foo\nbar") }
+
+          it 'includes the newline in the string segment' do
+            expect(segments).to eq([[:STRING, "foo\nbar"]])
+          end
+        end
+
+        context 'a variable and a suffix' do
+          let(:string) { '${foo}bar"' }
+
+          it 'puts the variable into an interpolation segment' do
+            expect(segments).to eq([
+              [:STRING, ''],
+              [:INTERP, 'foo'],
+              [:STRING, 'bar'],
+            ])
+          end
+        end
+
+        context 'a variable surrounded by text' do
+          let(:string) { 'foo${bar}baz"' }
+
+          it 'puts the variable into an interpolation segment' do
+            expect(segments).to eq([
+              [:STRING, 'foo'],
+              [:INTERP, 'bar'],
+              [:STRING, 'baz'],
+            ])
+          end
+        end
+
+        context 'multiple variables with surrounding text' do
+          let(:string) { 'foo${bar}baz${gronk}meh"' }
+
+          it 'puts each variable into an interpolation segment' do
+            expect(segments).to eq([
+              [:STRING, 'foo'],
+              [:INTERP, 'bar'],
+              [:STRING, 'baz'],
+              [:INTERP, 'gronk'],
+              [:STRING, 'meh'],
+            ])
+          end
+        end
+
+        context 'only an enclosed variable' do
+          let(:string) { '${bar}"' }
+
+          it 'puts empty string segments around the interpolated segment' do
+            expect(segments).to eq([
+              [:STRING, ''],
+              [:INTERP, 'bar'],
+              [:STRING, ''],
+            ])
+          end
+        end
+
+        context 'an enclosed variable with an unnecessary $' do
+          let(:string) { '${$bar}"' }
+
+          it 'does not remove the unnecessary $' do
+            expect(segments).to eq([
+              [:STRING, ''],
+              [:INTERP, '$bar'],
+              [:STRING, ''],
+            ])
+          end
+        end
+
+        context 'a variable with an array reference' do
+          let(:string) { '${foo[bar][baz]}"' }
+
+          it 'includes the references in the interpolated section' do
+            expect(segments).to eq([
+              [:STRING, ''],
+              [:INTERP, 'foo[bar][baz]'],
+              [:STRING, ''],
+            ])
+          end
+        end
+
+        context 'only enclosed variables' do
+          let(:string) { '${foo}${bar}"' }
+
+          it 'creates an interpolation section per variable' do
+            expect(segments).to eq([
+              [:STRING, ''],
+              [:INTERP, 'foo'],
+              [:STRING, ''],
+              [:INTERP, 'bar'],
+              [:STRING, ''],
+            ])
+          end
+        end
+
+        context 'an unenclosed variable' do
+          let(:string) { '$foo"' }
+
+          it 'creates a special segment for the unenclosed variable' do
+            expect(segments).to eq([
+              [:STRING, ''],
+              [:UNENC_VAR, '$foo'],
+              [:STRING, ''],
+            ])
+          end
+        end
+
+        context 'an interpolation with a nested single quoted string' do
+          let(:string) { %(string with ${'a nested single quoted string'} inside it") }
+
+          it 'creates an interpolation segment for the nested string' do
+            expect(segments).to eq([
+              [:STRING, 'string with '],
+              [:INTERP, "'a nested single quoted string'"],
+              [:STRING, ' inside it'],
+            ])
+          end
+        end
+
+        context 'an interpolation with nested math' do
+          let(:string) { 'string with ${(3+5)/4} nested math"' }
+
+          it 'creates an interpolation segment for the nested math' do
+            expect(segments).to eq([
+              [:STRING, 'string with '],
+              [:INTERP, '(3+5)/4'],
+              [:STRING, ' nested math'],
+            ])
+          end
+        end
+
+        context 'an interpolation with a nested array' do
+          let(:string) { %(string with ${['an array ', $v2]} in it") }
+
+          it 'creates an interpolation segment for the nested array' do
+            expect(segments).to eq([
+              [:STRING, 'string with '],
+              [:INTERP, "['an array ', $v2]"],
+              [:STRING, ' in it'],
+            ])
+          end
+        end
+
+        context 'repeated $s' do
+          let(:string) { '$$$$"' }
+
+          it 'creates a single string segment' do
+            expect(segments).to eq([[:STRING, '$$$$']])
+          end
+        end
+
+        context 'multiple unenclosed variables' do
+          let(:string) { '$foo$bar"' }
+
+          it 'creates a special segment for each unenclosed variable' do
+            expect(segments).to eq([
+              [:STRING, ''],
+              [:UNENC_VAR, '$foo'],
+              [:STRING, ''],
+              [:UNENC_VAR, '$bar'],
+              [:STRING, ''],
+            ])
+          end
+        end
+
+        context 'an unenclosed variable with a trailing $' do
+          let(:string) { 'foo$bar$"' }
+
+          it 'places the trailing $ in a string segment' do
+            expect(segments).to eq([
+              [:STRING, 'foo'],
+              [:UNENC_VAR, '$bar'],
+              [:STRING, '$'],
+            ])
+          end
+        end
+
+        context 'an unenclosed variable starting with two $s' do
+          let(:string) { 'foo$$bar"' }
+
+          it 'includes the preceeding $ in the string segment before the unenclosed variable' do
+            expect(segments).to eq([
+              [:STRING, 'foo$'],
+              [:UNENC_VAR, '$bar'],
+              [:STRING, ''],
+            ])
+          end
+        end
+
+        context 'an unenclosed variable with incorrect namespacing' do
+          let(:string) { '$foo::::bar"' }
+
+          it 'only includes the valid part of the variable name in the segment' do
+            expect(segments).to eq([
+              [:STRING, ''],
+              [:UNENC_VAR, '$foo'],
+              [:STRING, '::::bar'],
+            ])
+          end
+        end
+      end
+    end
+  end
+end

spec/puppet-lint/plugins/check_classes/variable_scope_spec.rb
@@ -313,7 +313,7 @@ describe 'variable_scope' do
     end
 
     it 'should create one warning' do
-      expect(problems).to contain_warning(msg).on_line(2).in_column(13)
+      expect(problems).to contain_warning(msg).on_line(2).in_column(14)
     end
   end
 end

spec/puppet-lint/plugins/check_strings/only_variable_string_spec.rb
@@ -12,7 +12,7 @@ describe 'only_variable_string' do
       end
 
       it 'should create a warning' do
-        expect(problems).to contain_warning(msg).on_line(1).in_column(3)
+        expect(problems).to contain_warning(msg).on_line(1).in_column(4)
       end
     end
 
@@ -24,7 +24,7 @@ describe 'only_variable_string' do
       end
 
       it 'should create a warning' do
-        expect(problems).to contain_warning(msg).on_line(1).in_column(3)
+        expect(problems).to contain_warning(msg).on_line(1).in_column(4)
       end
     end
 
@@ -36,7 +36,7 @@ describe 'only_variable_string' do
       end
 
       it 'should create a warning' do
-        expect(problems).to contain_warning(msg).on_line(1).in_column(3)
+        expect(problems).to contain_warning(msg).on_line(1).in_column(4)
       end
     end
 
@@ -73,7 +73,7 @@ describe 'only_variable_string' do
       end
 
       it 'should fix the manifest' do
-        expect(problems).to contain_fixed(msg).on_line(1).in_column(3)
+        expect(problems).to contain_fixed(msg).on_line(1).in_column(4)
       end
 
       it 'should unquote the variable' do
@@ -89,7 +89,7 @@ describe 'only_variable_string' do
       end
 
       it 'should fix the manifest' do
-        expect(problems).to contain_fixed(msg).on_line(1).in_column(3)
+        expect(problems).to contain_fixed(msg).on_line(1).in_column(4)
       end
 
       it 'should unquoted the variable' do
@@ -105,7 +105,7 @@ describe 'only_variable_string' do
       end
 
       it 'should fix the manifest' do
-        expect(problems).to contain_fixed(msg).on_line(1).in_column(3)
+        expect(problems).to contain_fixed(msg).on_line(1).in_column(4)
       end
 
       it 'should unquote the variable' do

commit 129b06e746b1921381d03d027061c5a0624c942c

Fix regression in tokenization in double-quoted strings
Fix regression introduced in commit
f8c002a2c6e11008c39e5926bdbb12d09afe8fa2 causing variables named
the same as Puppet language keywords (such as "${node}") to be
tokenized as their tokenized keyword (such as :NODE) and
not :VARIABLE.

lib/puppet-lint/lexer.rb
@@ -416,7 +416,7 @@ class PuppetLint
           lexer = PuppetLint::Lexer.new
           lexer.tokenise(segment[1])
           lexer.tokens.each_with_index do |t, i|
-            type = i.zero? && t.type == :NAME ? :VARIABLE : t.type
+            type = i.zero? && (t.type == :NAME || KEYWORDS.include?(t.type.to_s.downcase)) ? :VARIABLE : t.type
             tokens << new_token(type, t.value, :raw => t.raw)
           end
         when :UNENC_VAR

spec/puppet-lint/lexer_spec.rb
@@ -253,6 +253,47 @@ describe PuppetLint::Lexer do # rubocop:disable Metrics/BlockLength
       end
     end
 
+    context 'treats a variable named the same as the keyword as a variable' do
+      PuppetLint::Lexer::KEYWORDS.keys.each do |keyword|
+        context "for '#{keyword}'" do
+          let(:segments) do
+            [
+              [:STRING, ''],
+              [:INTERP, keyword],
+              [:STRING, ''],
+            ]
+          end
+
+          it 'creates a tokenised string' do
+            expect(tokens).to have(3).tokens
+
+            expect(tokens[0]).to have_attributes(
+              :type   => :DQPRE,
+              :value  => '',
+              :line   => 1,
+              :column => 1
+            )
+            expect(tokens[1]).to have_attributes(
+              :type   => :VARIABLE,
+              :value  => keyword,
+              :line   => 1,
+              :column => 4
+            )
+            expect(tokens[2]).to have_attributes(
+              :type   => :DQPOST,
+              :value  => '',
+              :line   => 1,
+              :column => keyword.size + 4
+            )
+          end
+
+          it 'can render the result back into a manifest' do
+            expect(manifest).to eq("\"${#{keyword}}\"")
+          end
+        end
+      end
+    end
+
     context 'an interpolated variable with an unnecessary $' do
       let(:segments) do
         [